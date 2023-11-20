6dabddf0617c5e376a8e69fbf2739867a9f47993
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-20 16:22:43 +0900

    Generate and display daily poem

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -2,13 +2,18 @@ app.controller("HomeController", [
   "$scope",
   "dailyPoem",
   function ($scope, dailyPoem) {
+    $scope.date = new Date();
     dailyPoem
       .then(function (data) {
-        $scope.poemData = data;
+        $scope.poemData = data[0];
       })
       .catch(function (error) {
         // Handle errors if needed
         console.error("Error fetching daily poem:", error);
       });
+
+    $scope.test = "testing controller variables";
   },
 ]);
+
+// parts of the json data title, author, lines, linecount
