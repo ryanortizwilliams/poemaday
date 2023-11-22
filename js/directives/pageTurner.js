eccd144ef456d06c6db79c577f5aa87584829cf2
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-23 00:25:01 +0900

    Create limit for paging index

--- js/directives/pageTurner.js
+++ js/directives/pageTurner.js
@@ -4,8 +4,24 @@ app.directive("pageTurner", function () {
     scope: { data: "=", index: "=" },
     templateUrl: "js/directives/pageTurner.html",
     link: function (scope, element, attrs) {
-      console.log(scope.data);
+      console.log(scope.data.length);
       scope.date = scope.data[scope.index].date;
+
+      //TODO write logic for routing urls limits
+      //check for first or final page
+      if (scope.index + 1 <= scope.data.length) {
+        scope.nextPage = scope.index + 1;
+      } else {
+        scope.nextPage = scope.index;
+      }
+
+      if (scope.index - 1 >= scope.data.length) {
+        scope.nextPage = scope.index - 1;
+      } else {
+        scope.nextPage = scope.index;
+      }
+
+      scope.previousPage = scope.index - 1;
     },
   };
 });
