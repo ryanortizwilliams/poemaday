40d99bc560805e1330edc95f3008a841736e9e91
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-23 00:01:49 +0900

    Add data to entry page

--- js/controllers/EntryController.js
+++ js/controllers/EntryController.js
@@ -2,7 +2,11 @@ app.controller("EntryController", [
   "$scope",
   "$routeParams",
   function ($scope, $routeParams) {
-    $scope.test = "testing";
-    $scope.passedParam = $routeParams.id;
+    $scope.index = $routeParams.id;
+    $scope.poemLibrary = JSON.parse(localStorage.getItem("poemLibrary"));
+    $scope.reviewLibrary = JSON.parse(localStorage.getItem("reviewLibrary"));
+
+    $scope.poemData = $scope.poemLibrary[$scope.index].poem;
+    $scope.impressionsText = $scope.reviewLibrary[$scope.index];
   },
 ]);
