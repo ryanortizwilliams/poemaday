63f0b1121ea5453cce11a40e914b357c3fb2f05e
ryanortizwilliams <ryanortizwilliams@outlook.com>
2023-11-22 14:02:04 +0900

    Filter order in reverse. Refactor

--- js/controllers/OverviewController.js
+++ js/controllers/OverviewController.js
@@ -1,8 +1,6 @@
 app.controller("OverviewController", [
   "$scope",
   function ($scope) {
-    $scope.testVar = "testing testing";
-
     $scope.poemLibrary = JSON.parse(localStorage.getItem("poemLibrary"));
     $scope.reviewLibrary = JSON.parse(localStorage.getItem("reviewLibrary"));
   },
